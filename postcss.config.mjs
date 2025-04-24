478c9eab9d851cb557748eb6a3990ed23b8db364
Shivam Kumar <shivam177@gmail.com>
2025-04-24 22:04:49 +0530

    fifth

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,5 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-  ]
-  
 };
 
 export default config;
